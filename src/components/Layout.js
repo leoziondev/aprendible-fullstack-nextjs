8ca047faa46e19485dae2c1915d05b2f69375025
LeoZion <leozionsuporte@gmail.com>
2022-11-03 02:11:21 -0300

    add link router back

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,13 +1,28 @@
 import Link from "next/link"
 import Logo from "./Logo"
 
+import { ArrowLeft } from 'phosphor-react'
+import Container from "./ui/Container"
+import { useRouter } from "next/router"
+
 const Layout = ({ children }) => {
+  const router = useRouter()
+
+  const handleBack = () => {
+    router.back()
+  }
+
   return (
     <div className="bg-slate-900 flex flex-col min-h-screen">
       <header>
-        <Link href="/" className="flex flex-col items-center justify-center pt-4 mb-8">
-          <Logo sizeIcon={32} className="text-2xl"/>
-        </Link>
+        <Container className="relative">
+          <button onClick={handleBack} className="absolute top-10" title="Voltar">
+            <ArrowLeft size={18} className="text-white" />
+          </button>
+          <Link href="/" className="flex flex-col items-center justify-center pt-4 mb-8">
+            <Logo sizeIcon={32} className="text-2xl"/>
+          </Link>
+        </Container>
       </header>
       <main>
         {children}
