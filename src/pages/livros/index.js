b97be0b0ff14d467e6d6b67717df115f6745e7da
LeoZion <leozionsuporte@gmail.com>
2022-11-03 02:12:20 -0300

    get data all books with static props

--- src/pages/livros/index.js
+++ src/pages/livros/index.js
@@ -5,11 +5,12 @@ import Container from "../../components/ui/Container"
 import Title from "../../components/Title"
 
 import { Book } from 'phosphor-react'
+import { api } from "../../lib/api"
 
-const Books = () => {
+const Books = ({ books }) => {
   return (
     <Layout>
-      <Container className="flex items-center justify-between">
+      <Container className="flex items-center justify-between mb-4">
         <Title text="Livros" />
         <Link
           href="/livros/criar"
@@ -19,8 +20,31 @@ const Books = () => {
           Criar livro
         </Link>
       </Container>
+      <Container>
+        <ul>
+          {books.map((book) => {
+            return (
+              <li key={book.id} className="bg-slate-800 rounded-md py-2 px-4 mb-2 text-slate-500 hover:bg-indigo-500 hover:text-indigo-100">
+                <Link href={`/livros/${book.id}`}>
+                  <h2>{book.title}</h2>
+                </Link>
+              </li>
+            )
+          })}
+        </ul>
+      </Container>
     </Layout>
   )
+}
+
+export const getStaticProps = async () => {
+  const res = await api.get('/api/books')
+
+  return {
+    props: {
+      books: res.data
+    }
+  }
 }
 
 export default Books
