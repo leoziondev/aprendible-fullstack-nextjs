d8bd2cf932fa9d43d8cee59faa034b1b9e7eeea0
LeoZion <leozionsuporte@gmail.com>
2022-11-03 02:15:53 -0300

    get data book id with static props and static paths

--- src/pages/livros/[bid]/index.js
+++ src/pages/livros/[bid]/index.js
@@ -1,15 +1,41 @@
+import { api } from "../../../lib/api"
+
 import Layout from "../../../components/Layout"
 import Title from "../../../components/Title"
 import Container from "../../../components/ui/Container"
 
-const BookDetails = () => {
+
+
+const BookDetails = ({ book }) => {
   return (
-    <Layout>
+    <Layout>      
       <Container>
-        <Title text="Detalhes do Livro" />
+        <Title text={book.title} />
       </Container>
     </Layout>
   )
+}
+
+export const getStaticPaths = async () => {
+  const res = await api.get('/api/books')
+
+  return {
+    paths: res.data.map((book) => ({
+      params: { bid: String(book.id) }
+    })),
+    fallback: false
+  }
+}
+
+export const getStaticProps = async ({ params }) => {
+  const bid = params.bid
+  const res = await api.get(`/api/books/${bid}`)
+
+  return {
+    props: {
+      book: res.data
+    }
+  }
 }
 
 export default BookDetails
